78852ebf2ff537695e5dc8d099cba0b6111e5a16
Yang Li <liyangyijie@gmail.com>
2014-10-07 14:18:59 +0800

    Update debian-shadowsocks-libev.sh

--- debian-shadowsocks-libev.sh
+++ debian-shadowsocks-libev.sh
@@ -47,7 +47,7 @@ fi
 # install
 apt-get update -y
 apt-get upgrade -y
-apt-get install build-essential autoconf libtool libssl-dev gcc -y
+apt-get install build-essential autoconf libtool libssl-dev gcc vim -y
 apt-get install git -y
 git clone https://github.com/madeye/shadowsocks-libev.git
 cd shadowsocks-libev 
