8cd5220b44355d9c25dfc174789ed1fcc6fdfc6a
Yang Li <liyangyijie@users.noreply.github.com>
2015-01-26 11:33:45 +0800

    Update SSLVPNauto-L-0.2.sh

--- test/SSLVPNauto-L-0.2.sh
+++ test/SSLVPNauto-L-0.2.sh
@@ -15,13 +15,13 @@ echo "# Install  OpenConnect VPN server for Debian 7+"
 echo "#############################################################"
 echo ""
 
-# install 
+# install 安装主体
 function install_OpenConnect_VPN_server(){
     
-#check system and get IP port
+#check system , get IP and port ,del test sources 检测系统 获取本机公网ip、默认验证端口 去除测试源
     check_Required
 	
-#custom-configuration or not
+#custom-configuration or not 自定义安装与否
     print_info "Do you want to install ocserv with Custom Configuration?(y/n)"
     read -p "(Default :n):" Custom_config_ocserv
     if [ "$Custom_config_ocserv" = "y" ]; then
@@ -31,8 +31,11 @@ function install_OpenConnect_VPN_server(){
 	else
 	print_info "Automatic installation."
     fi
+
+#add a user 增加初始一个用户
+    add_a_user
 	
-#press any key to star
+#press any key to start 任意键开始
     get_char(){
         SAVEDSTTY=`stty -g`
         stty -echo
@@ -45,27 +48,35 @@ function install_OpenConnect_VPN_server(){
     echo ""
     print_info "press any key to start...or Press Ctrl+C to cancel"
     ocserv_char=`get_char`
-    	
+
+#install dependencies 安装依赖文件
 	pre_install
 	
+#install ocserv 编译安装软件	
 	tar_ocserv_install
-	
+
+#make self-signd server-ca 制作服务器自签名证书	
 	if [ "$self_signed_ca" = "" ]; then
 	make_ocserv_ca
 	fi
-	
+
+#test 证书登录 测试中	
 	if [ "$ca_login" = "y" ]; then
 	ca_login_ocserv	
 	fi
-	
+
+#configuration 设定软件相关选项	
 	set_ocserv_conf
-	
+
+#stop all 关闭所有正在运行的ocserb软件	
 	stop_ocserv
-	
+
+#No certificate , then do not start 没有服务器证书不启动	
 	if [ "$self_signed_ca" = "" ]; then	
 	start_ocserv
 	fi
-	
+
+#show result 显示结果	
 	show_ocserv    
 }
 
@@ -74,6 +85,7 @@ function reinstall_ocserv {
 	rm -rf /etc/ocserv
 	rm -rf /etc/dbus-1/system.d/org.infradead.ocserv.conf
 	rm -rf /usr/sbin/ocserv
+	rm -rf /etc/init.d/ocserv
 	install_OpenConnect_VPN_server
 }
 
@@ -107,20 +119,28 @@ function check_Required {
 	    die "ocserv has been installed!!!"
 	fi
 	print_info "not installed ok"
-	#get own IPv4 info
-	print_info "getting ip ......"
+	#get own IPv4 info 
+	print_info "getting ip from net......"
 	apt-get update  -qq
-	apt-get install -qq -y vim sudo gawk wget curl nano sed
+	apt-get install -qq -y vim sudo gawk curl nano sed
         ocserv_hostname=$(wget -qO- ipv4.icanhazip.com)
     if [ $? -ne 0 -o -z $ocserv_hostname ]; then
         ocserv_hostname=`curl -s liyangyijie.sinaapp.com/ip/`
-    fi
+        fi
 	print_info "get ip ok"
 	#get default port
-	print_info "getting default port ......"
-	ocserv_tcpport_Default=$(wget -qO- --no-check-certificate https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocserv.conf | grep '^tcp-port' | sed 's/tcp-port = //g')
-	ocserv_udpport_Default=$(wget -qO- --no-check-certificate https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocserv.conf | grep '^udp-port' | sed 's/udp-port = //g')
+	print_info "getting default port from net......"
+	ocserv_tcpport_Default=$(wget -qO- --no-check-certificate https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocservauto/ocserv.conf | grep '^tcp-port' | sed 's/tcp-port = //g')
+	ocserv_udpport_Default=$(wget -qO- --no-check-certificate https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocservauto/ocserv.conf | grep '^udp-port' | sed 's/udp-port = //g')
 	print_info "get default port ok"
+	#sources check ,del this sources 
+	cat /etc/apt/sources.list | grep 'deb ftp://ftp.debian.org/debian/ jessie main contrib non-free' > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+	oc_jessie="n"
+	else
+        sed -i 's@deb ftp://ftp.debian.org/debian/ jessie main contrib non-free@@g' /etc/apt/sources.list
+    fi
+	print_info "sources ok"
 	clear
 }
 #error and force-exit
@@ -218,6 +238,19 @@ fi
     
     echo "####################################"
 	
+#Boot from the start
+    print_info "Boot from the start?(y/n)"
+    read -p "(Default :y):" ocserv_boot_start
+    if [ "$ocserv_boot_start" = "n" ]; then
+        ocserv_boot_start="n"
+	print_info "Do not boot from the start!"	
+    else
+	    ocserv_boot_start=""
+    print_info "Boot from the start!"
+	fi
+    echo "####################################" 
+
+	
 #Whether to use the certificate login
 	print_info "Whether to use the certificate login?(y/n)"
 	read -p "(Default :n):" ca_login 
@@ -238,12 +271,47 @@ else
 fi    
     echo "####################################"
 }
+function add_a_user(){
+# Get username
+    print_info "Input your username for ocserv:"
+    read -p "(Default :123456):" username
+    if [ "$username" = "" ]; then
+        username="123456"
+    fi
+    print_info "Your username:$username"
+    echo "####################################"
+# Get password
+    print_info "Input your password for ocserv:"
+    read -p "(Default :123456):" password
+    if [ "$password" = "" ]; then
+        password="123456"
+    fi
+    print_info "Your password:$password"
+    echo "####################################"
+
+}
 
 # pre_install
 function pre_install(){
    #keep kernel
    echo linux-image-`uname -r` hold | sudo dpkg --set-selections
    sudo apt-get upgrade -y
+   #no update from test sources
+   if [ ! -d /etc/apt/preferences.d ];then
+       mkdir /etc/apt/preferences.d
+   fi
+   cat > /etc/apt/preferences.d/my_ocserv_preferences<<EOF
+Package: *
+Pin: release wheezy
+Pin-Priority: 900
+Package: *
+Pin: release wheezy-backports
+Pin-Priority: 90
+Package: *
+Pin: release jessie
+Pin-Priority: 60
+EOF
+ 
    #sources check, Do not change the order
    cat /etc/apt/sources.list | grep 'deb http://ftp.debian.org/debian wheezy-backports main contrib non-free' > /dev/null 2>&1
    if [ $? -ne 0 ]; then
@@ -252,23 +320,21 @@ function pre_install(){
    fi
    
    apt-get update
-#  apt-get install -y libprotobuf-c0-dev
+   apt-get install -y libprotobuf-c0-dev
    apt-get install -y libreadline6 libreadline5 libreadline6-dev libgmp3-dev m4 gcc pkg-config make gnutls-bin libtalloc-dev build-essential libwrap0-dev libpam0g-dev libdbus-1-dev libreadline-dev libnl-route-3-dev libpcl1-dev libopts25-dev autogen libseccomp-dev libnl-nf-3-dev debhelper
    apt-get install -y -qq libreadline6 libreadline5 libreadline6-dev libgmp3-dev m4 gcc pkg-config make gnutls-bin libtalloc-dev build-essential libwrap0-dev libpam0g-dev libdbus-1-dev libreadline-dev libnl-route-3-dev libpcl1-dev libopts25-dev autogen libseccomp-dev libnl-nf-3-dev debhelper
    apt-get install -y -t wheezy-backports  libgnutls28-dev
    apt-get install -y -qq -t wheezy-backports  libgnutls28-dev
    
-   
-   cat /etc/apt/sources.list | grep 'deb ftp://ftp.debian.org/debian/ jessie main contrib non-free' > /dev/null 2>&1
-   if [ $? -ne 0 ]; then
+   #sources check @ check Required
+
    echo "deb ftp://ftp.debian.org/debian/ jessie main contrib non-free" >> /etc/apt/sources.list 
-   oc_jessie="n"
-   fi
+  
    
-   #update dependencies
-   apt-get update
-   apt-get install -y -t jessie  libprotobuf-c-dev libhttp-parser-dev
-   apt-get install -y -qq -t jessie  libprotobuf-c-dev libhttp-parser-dev
+   #update dependencies too new ~
+   #apt-get update
+   #apt-get install -y -t jessie  libprotobuf-c-dev libhttp-parser-dev
+   #apt-get install -y -qq -t jessie  libprotobuf-c-dev libhttp-parser-dev
    
    #if sources del
    if [ "$oc_wheezy_backports" = "n" ]; then
@@ -280,6 +346,7 @@ function pre_install(){
    fi
    
    #keep update
+   rm -rf /etc/apt/preferences.d/my_ocserv_preferences
    apt-get update
    
    print_info "dependencies  ok"
@@ -308,24 +375,20 @@ function tar_ocserv_install(){
 # get config file from net
   
    cd /etc/ocserv
-   wget https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocserv.conf --no-check-certificate
-   wget https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocserv-sysctl.sh  --no-check-certificate
-   chmod +x ocserv-sysctl.sh
+   wget https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocservauto/ocserv.conf --no-check-certificate
+   wget https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocservauto/start-ocserv-sysctl.sh  --no-check-certificate
+   wget https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocservauto/stop-ocserv-sysctl.sh  --no-check-certificate
+   wget https://raw.githubusercontent.com/fanyueciyuan/useful/master/ocservauto/ocserv  --no-check-certificate
+   chmod 755 ocserv
+   mv ocserv /etc/init.d
+   chmod +x start-ocserv-sysctl.sh
+   chmod +x stop-ocserv-sysctl.sh
    touch ocpasswd
    chmod 600 ocpasswd
    
    print_info "ocserv install ok"
 }
-function git_ocserv_install(){
-   cd ~
-   git clone git://git.infradead.org/ocserv.git
-   cd ocserv
-   git submodule update --init
-   bash autogen.sh
-   ./configure --prefix=/usr --sysconfdir=/etc && make && make install
-   cd ~
-   rm -rf ocserv
-}
+
 function make_ocserv_ca(){
 #all in one doc
 cd /etc/ocserv/CAforOC
@@ -404,6 +467,13 @@ function set_ocserv_conf(){
 #default domain 
 sed -i "s@#default-domain = example.com@default-domain = $fqdnname@" /etc/ocserv/ocserv.conf 
   
+#Boot from the start 
+if [ "$ocserv_boot_start" = "" ]; then
+sudo update-rc.d ocserv defaults
+fi
+
+#add a user
+(echo "$password"; sleep 1; echo "$password") | ocpasswd -c "/etc/ocserv/ocpasswd" $username
 
 #set ca_login
   if [ "$ca_login" = "y" ]; then
@@ -435,14 +505,8 @@ if [ ! -f /etc/ocserv/server-cert.pem ] || [ ! -f /etc/ocserv/server-key.pem ]; 
 fi
 
 #start
-bash /etc/ocserv/ocserv-sysctl.sh
-#Add run on system start up
-cat /etc/rc.local | grep 'bash /etc/ocserv/ocserv-sysctl.sh' > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-       cp -rpf /etc/rc.local /opt/rc.local_no_ocservbak
-       sed -i "/By default this script does nothing./a\bash /etc/ocserv/ocserv-sysctl.sh" /etc/rc.local
-       
-fi
+
+/etc/init.d/ocserv start
 }
 function show_ocserv(){
 ocserv_port=`cat /etc/ocserv/ocserv.conf | grep '^tcp-port' | sed 's/tcp-port = //g'`
@@ -458,16 +522,21 @@ if [ $? -eq 0 ]; then
 	else
 	echo ""
     echo -e "\033[41;37m Your server domain is \033[0m" "$fqdnname:$ocserv_port"
+	echo -e "\033[41;37m Your username is \033[0m" "$username"
+	echo -e "\033[41;37m Your password is \033[0m" "$password"
     echo -e "\033[41;37m You can use 'sudo ocpasswd -c /etc/ocserv/ocpasswd username' to add users. \033[0m "
     echo ""    
     print_info "enjoy it!"
     echo ""
 	fi
 elif [ "$self_signed_ca" = "n" -a "$ca_login" = "" ]; then    
-	print_warn "1,You have to put your CA and Key to /etc/ocserv !!!"
-	print_warn "2,You have to change your CA and Key'name to server-cert.pem and server-key.pem !!!"
-	print_warn "3,You have to use 'sudo ocpasswd -c /etc/ocserv/ocpasswd username' to add users. "
-	print_warn "4,You have to start ocserv by this script!"
+	print_warn "1,You have to change your CA and Key'name to server-cert.pem and server-key.pem !!!"
+	print_warn "2,You have to put your CA and Key to /etc/ocserv !!!"
+	print_warn "3,You have to start ocserv by '/etc/init.d/ocserv start'!"
+	print_warn "4,You can use 'sudo ocpasswd -c /etc/ocserv/ocpasswd username' to add users."
+	echo -e "\033[41;37m Your username is \033[0m" "$username"
+	echo -e "\033[41;37m Your password is \033[0m" "$password"
+	
 elif [ "$self_signed_ca" = "n" -a "$ca_login" = "y" ]; then  
     echo "test 2"
 else
