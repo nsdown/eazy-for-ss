dedbe53b3112d586a7148e6fae5002facc2ebeff
Yang Li <liyangyijie@users.noreply.github.com>
2015-04-13 23:56:55 +0800

    debug

--- ocservauto/Speeder_reload.sh
+++ ocservauto/Speeder_reload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wanif=`ip a|awk '{print $NF}|grep vpns|sed ':a;N;s/\n/ /;ba;''`
+wanif=`ip a|awk '{print $NF}'|grep vpns|sed ':a;N;s/\n/ /;ba;'`
 [ "$wanif" = "" ] || wanif=" $wanif"
 gwif=`ip route show|sed -n 's/^default.* dev \([^ ]*\).*/\1/p'`
 sed -i "s/^accif=.*/accif=\"${gwif}${wanif}\"/" /serverspeeder/etc/config
