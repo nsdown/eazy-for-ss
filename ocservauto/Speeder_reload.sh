5cdc520efb46e32a3bde5cb95ade81908914b6e3
Yang Li <liyangyijie@users.noreply.github.com>
2015-04-13 20:51:46 +0800

    Create Speeder_reload.sh

---
+++ ocservauto/Speeder_reload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wanif=`ip a|awk '{print $NF}|grep vpns|sed ':a;N;s/\n/ /;ba;''`
+[ "$wanif" = "" ] || wanif=" $wanif"
+gwif=`ip route show|sed -n 's/^default.* dev \([^ ]*\).*/\1/p'`
+sed -i "s/^accif=.*/accif=\"${gwif}${wanif}\"/" /serverspeeder/etc/config
+sleep 1
+/serverspeeder/bin/serverSpeeder.sh reload
