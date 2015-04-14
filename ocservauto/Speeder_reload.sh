64d172c41993e4aa322f635d770633393e0141e9
Yang Li <liyangyijie@users.noreply.github.com>
2015-04-14 17:54:36 +0800

    Update Speeder_reload.sh

--- ocservauto/Speeder_reload.sh
+++ ocservauto/Speeder_reload.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-wanif=`ip a|awk '{print $NF}'|grep vpns|sed ':a;N;s/\n/ /;ba;'`
+OC_CONFIG="/etc/ocserv/ocserv.conf"
+device=`sed -n 's/^device.*=[ \t]*//p' $OC_CONFIG`
+wanif=`ip a|awk '{print $NF}'|grep $device|sed ':a;N;s/\n/ /;ba;'`
 [ "$wanif" = "" ] || wanif=" $wanif"
 gwif=`ip route show|sed -n 's/^default.* dev \([^ ]*\).*/\1/p'`
 sed -i "s/^accif=.*/accif=\"${gwif}${wanif}\"/" /serverspeeder/etc/config
