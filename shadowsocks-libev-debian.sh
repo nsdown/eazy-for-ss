9b50e73da0295366d406bacae23c57b7aed0b665
Yang Li <liyangyijie@users.noreply.github.com>
2015-04-27 16:28:20 +0800

    +Check_Tcp_Port+apt-transport-https+net-tools

--- shadowsocks-libev-debian.sh
+++ shadowsocks-libev-debian.sh
@@ -69,8 +69,10 @@ fi
 function pre_install(){
 #base-tool
 apt-get update
+echo linux-image-`uname -r` hold | sudo dpkg --set-selections
 apt-get upgrade -y
-apt-get install -y -qq sudo nano sed vim gawk curl dnsutils
+echo linux-image-`uname -r` install | sudo dpkg --set-selections
+apt-get install -y -qq sudo nano sed vim gawk curl dnsutils apt-transport-https net-tools
 #tcp choice
 #load hybla
 /sbin/modprobe tcp_hybla > /dev/null 2>&1
@@ -82,38 +84,56 @@ tcp_congestion_ss="cubic"
 fi
 #sysctl file
 cat > /etc/sysctl.d/local_ss.conf<<EOF
-
+# max open files
 fs.file-max = 51200
-
+# max read buffer
 net.core.rmem_max = 67108864
+# max write buffer
 net.core.wmem_max = 67108864
+# default read buffer
 net.core.rmem_default = 65536
+# default write buffer
 net.core.wmem_default = 65536
+# max processor input queue
 net.core.netdev_max_backlog = 4096
+# max backlog
 net.core.somaxconn = 4096
 
+# resist SYN flood attacks
 net.ipv4.tcp_syncookies = 1
+# reuse timewait sockets when safe
 net.ipv4.tcp_tw_reuse = 1
+# turn off fast timewait sockets recycling
 net.ipv4.tcp_tw_recycle = 0
+# short FIN timeout
 net.ipv4.tcp_fin_timeout = 30
+# short keepalive time
 net.ipv4.tcp_keepalive_time = 1200
+# outbound port range
 net.ipv4.ip_local_port_range = 10000 65000
+# max SYN backlog
 net.ipv4.tcp_max_syn_backlog = 4096
+# max timewait sockets held by system simultaneously
 net.ipv4.tcp_max_tw_buckets = 5000
+# turn on TCP Fast Open on both client and server side
 net.ipv4.tcp_fastopen = 3
+# TCP receive buffer
 net.ipv4.tcp_rmem = 4096 87380 67108864
+# TCP write buffer
 net.ipv4.tcp_wmem = 4096 65536 67108864
+# turn on path MTU discovery
 net.ipv4.tcp_mtu_probing = 1
+
 net.ipv4.tcp_congestion_control = $tcp_congestion_ss
 EOF
 #sysctl set
 sysctl -p /etc/sysctl.d/local_ss.conf
 #+source   
-cat /etc/apt/sources.list|grep -v '^#'|grep 'deb http://shadowsocks.org/debian' > /dev/null 2>&1
+sed 's/^[ \t]*//' /etc/apt/sources.list|grep -v '^#'|grep 'shadowsocks' > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-    expr $(cat /etc/debian_version|cut -d. -f1|grep -v ^#) + 0 > /dev/null 2>&1
+    expr $(cat /etc/debian_version|cut -d. -f1) + 0 > /dev/null 2>&1
     if [ $? -eq 0 ]; then
-        D_V=`expr $(cat /etc/debian_version|cut -d. -f1|grep -v ^#)`
+        D_V=`expr $(cat /etc/debian_version|cut -d. -f1)`
         if [ $D_V -lt 7 ]; then        
             echo "deb http://shadowsocks.org/debian squeeze main" >> /etc/apt/sources.list
         else
@@ -126,6 +146,17 @@ if [ $? -ne 0 ]; then
     wget -O- http://shadowsocks.org/debian/1D27208A.gpg | sudo apt-key add -
 fi
 clear
+}
+
+function Check_Tcp_Port(){
+All_Listen_Tcp_Port=`netstat -napt|grep -i 'listen'|awk {'print $4'}|sed 's/.*:\(.*\)/\1/'|sort|uniq`
+Port=""
+for Port in $All_Listen_Tcp_Port
+do
+    if [ "$1" = "$Port" ]; then
+        return 1
+    fi
+done
 }
 
 function get_config(){
@@ -144,6 +175,15 @@ if [ "$shadowsockspt" = "" ]; then
     shadowsockspt="443"
 fi
 echo "port:$shadowsockspt"
+while !(Check_Tcp_Port "$shadowsockspt"); do
+echo "The port is in use , please choose a different port!"
+echo "Please input server port for shadowsocks-libev:"
+read -p "(Default port: 443):" shadowsockspt
+if [ "$shadowsockspt" = "" ]; then
+    shadowsockspt="443"
+fi
+echo "port:$shadowsockspt"
+done
 echo "####################################"
 # Get shadowsocks-libev config Encryption Method
 echo "Please input Encryption Method for shadowsocks-libev:"
@@ -153,7 +193,7 @@ if [ "$shadowsocksem" = "" ]; then
 fi
 echo "encryption method:$shadowsocksem"
 echo "####################################"
-#any key go on	
+#any key go on
 get_char(){
     SAVEDSTTY=`stty -g`
     stty -echo
@@ -170,17 +210,14 @@ ss_char=`get_char`
 }
 
 function shadowsocks_update(){
-echo linux-image-`uname -r` hold | sudo dpkg --set-selections
-sudo apt-get update
+apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install shadowsocks-libev -y
 sed -i 's|\(MAXFD=\).*|\151200|' /etc/default/shadowsocks-libev
 }
 
 function config_shadowsocks(){
 # set config 
-if [ ! -d /etc/shadowsocks-libev ];then
-    mkdir /etc/shadowsocks-libev
-fi
+[ ! -d /etc/shadowsocks-libev ] && mkdir /etc/shadowsocks-libev
 #only 0.0.0.0
 cat > /etc/shadowsocks-libev/config.json<<EOF
 {
@@ -194,7 +231,7 @@ EOF
 }
 
 function stop_shadowsocks(){
-sudo /etc/init.d/shadowsocks-libev stop
+/etc/init.d/shadowsocks-libev stop
 #stop all
 ss_pid=`pidof ss-server`
 if [ ! -z "$ss_pid" ]; then
@@ -210,7 +247,7 @@ fi
 
 function stop_start_shadowsocks(){
 stop_shadowsocks
-sudo /etc/init.d/shadowsocks-libev start
+/etc/init.d/shadowsocks-libev start
 }
 
 function show_shadowsocks(){
@@ -235,7 +272,7 @@ if [ $? -eq 0 ]; then
     exit
 else
     echo "Shadowsocks-libev start failure!"
-	exit
+    exit
 fi
 }
 
