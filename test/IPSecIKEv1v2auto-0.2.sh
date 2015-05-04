28d724c08b7a66f732598f97921d2a9f0b137bf3
Yang Li <liyangyijie@users.noreply.github.com>
2015-05-04 22:30:18 +0800

    random^ yesorno^

--- test/IPSecIKEv1v2auto-0.2.sh
+++ test/IPSecIKEv1v2auto-0.2.sh
@@ -41,14 +41,16 @@ function Default_Ask(){
     Temp_default_var=$2
     Temp_var_name=$3
 #if yes or no 
-    echo -e -n "\e[1;36m$Temp_question\e[0m""\033[31m(Default:$Temp_default_var): \033[0m"
+    echo -e -n "\e[1;36m$Temp_question\e[0m""\033[31m(Default:$Temp_default_var)\033[0m"
+    echo
     read Temp_var
     if [ "$Temp_default_var" = "y" ] || [ "$Temp_default_var" = "n" ] ; then
+        Temp_var=$(echo $Temp_var | sed 'y/YESNO0/yesnoo/')
         case $Temp_var in
-            y|Y|Yes|YES|yes|yES|yEs|YeS|yeS)
+            y|ye|yes)
                 Temp_var=y
                 ;;
-            n|N|No|NO|no|nO)
+            n|no)
                 Temp_var=n
                 ;;
             *)
@@ -60,7 +62,6 @@ function Default_Ask(){
     fi
     Temp_cmd="$Temp_var_name='$Temp_var'"
     eval $Temp_cmd
-    echo
     print_info "Your answer is : ${Temp_var}"
     echo
     print_xxxx
@@ -81,15 +82,11 @@ function press_any_key {
     get_char_fffff=`get_char_ffff`
     echo
 }
-#get random word
+#get random word 获取$1位随机文本，剔除容易识别错误的字符例如0和O等等
 function get_random_word(){
-    index=0
-    str=""
-    for i in {a..z}; do arr[index]=$i; index=$(expr ${index} + 1); done
-    for i in {A..Z}; do arr[index]=$i; index=$(expr ${index} + 1); done
-    for i in {0..9}; do arr[index]=$i; index=$(expr ${index} + 1); done
-#$1 figures
-    for i in `seq 1 $1`; do str="$str${arr[$RANDOM%$index]}"; done
+    D_Num_Random="8"
+    Num_Random=${1:-$D_Num_Random}
+    str=`cat /dev/urandom | tr -cd abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789 | head -c $Num_Random`
     echo $str
 }
 
